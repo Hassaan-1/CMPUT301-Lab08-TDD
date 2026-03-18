c8ee8d930a70c383432b1baf1cf637afffbcfa69
Hassaan-1 <hassaanzafar8787@gmail.com>
2026-03-19 04:51:39 +0500

    RED phase: add failing test for deleteCity()

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -13,4 +13,13 @@ public class CustomListTest {
 
         assertTrue(list.hasCity(calgary));
     }
+    @Test
+    public void testDeleteCity() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+
+        list.deleteCity(calgary); // method does NOT exist yet
+        assertFalse(list.hasCity(calgary));
+    }
 }
